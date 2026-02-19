3258412dafeb01f6d6e13fbefd74c35fe0f6704f
Amit_Divate <adivate49@gmail.com>
2026-02-19 18:20:48 +0530

    pipeline and k8s-deployment file

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,5 +6,41 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/DIVATEe/flight-reservation-app.git'
             }
         }
+        stage('Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package
+                '''
+            }
+        }
+        stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                  sh'''
+                     cd FlightReservationApplication
+                     mvn sonar:sonar -Dsonar.projectKey=flight-reservation   
+                  ''' 
+                }
+            }
+        }
+        stage('Docker-build'){
+            steps{
+                sh'''
+                    cd FlightReservationApplication
+                    docker build -t divatee/flight-reservation-1:latest . 
+                    docker push divatee/flight-reservation-1:latest
+                    docker rmi divatee/flight-reservation-1:latest
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                sh'''
+                    cd FlightReservationApplication   
+                    kubectl apply -f k8s/
+                '''
+            }
+        }
     }
 }
