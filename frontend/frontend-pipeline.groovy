c462e08bae3f487750c50f74812a744227ec7359
Amit_Divate <adivate49@gmail.com>
2026-02-20 15:46:35 +0530

    changes to frontend pipeline

--- frontend/frontend-pipeline.groovy
+++ frontend/frontend-pipeline.groovy
@@ -19,7 +19,7 @@ pipeline{
             steps{
                 sh '''
                     cd frontend
-                    aws s3 sync dist/ s3://cblkdfsfdsc-front12end-project-bux/
+                    aws s3 sync dist/ s3://cbz-frontend-amit-1422/
                 '''    
             }
         }
